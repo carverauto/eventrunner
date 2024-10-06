2778270387a0fc0ee5a797a63eded7dcd3ac8f7d
Michael Freeman <mfreeman@threadr.ai>
2024-10-06 12:56:50 -0500

    refactored some http middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/carverauto/eventrunner/cmd/api/migrations"
 	"github.com/carverauto/eventrunner/pkg/api/handlers"
-	middlewarePkg "github.com/carverauto/eventrunner/pkg/api/middleware"
+	"github.com/carverauto/eventrunner/pkg/api/middleware"
 	"gofr.dev/pkg/gofr"
 	"gofr.dev/pkg/gofr/datasource/mongo"
 )
@@ -23,40 +23,17 @@ func main() {
 	userHandler := &handlers.UserHandler{}
 
 	// Tenant routes (protected by API key)
-	app.POST("/tenants", adapt(tenantHandler.Create, middleware.AuthenticateAPIKey))
-	app.GET("/tenants", adapt(tenantHandler.GetAll, middleware.AuthenticateAPIKey))
+	app.POST("/tenants", middleware.Adapt(tenantHandler.Create, middleware.AuthenticateAPIKey))
+	app.GET("/tenants", middleware.Adapt(tenantHandler.GetAll, middleware.AuthenticateAPIKey))
 
 	// User routes (protected by API key and role-based access)
-	app.POST("/tenants/{tenant_id}/users", adapt(userHandler.Create,
+	app.POST("/tenants/{tenant_id}/users", middleware.Adapt(userHandler.Create,
 		middleware.AuthenticateAPIKey,
 		middleware.RequireRole("admin")))
-	app.GET("/tenants/{tenant_id}/users", adapt(userHandler.GetAll,
+	app.GET("/tenants/{tenant_id}/users", middleware.Adapt(userHandler.GetAll,
 		middleware.AuthenticateAPIKey,
 		middleware.RequireRole("admin", "user")))
 
 	// Run the application
 	app.Run()
-}
-
-// adapt converts a handler func and middlewares into a gofr.Handler
-func adapt(h interface{}, middlewares ...handlers.Middleware) gofr.Handler {
-	return func(c *gofr.Context) (interface{}, error) {
-		var handler handlers.Handler
-		switch h := h.(type) {
-		case func(*gofr.Context) (interface{}, error):
-			handler = handlers.HandlerFunc(h)
-		default:
-			if h, ok := h.(handlers.Handler); ok {
-				handler = h
-			} else {
-				panic("unsupported handler type")
-			}
-		}
-
-		for i := len(middlewares) - 1; i >= 0; i-- {
-			handler = middlewares[i](handler)
-		}
-
-		return handler.Handle(c)
-	}
 }
