ef677fd7e94a79bc078b049e74f881d547e21193
mfreeman451 <mfreeman451@gmail.com>
2024-10-26 13:39:42 -0500

    sync

--- cmd/auth-server/main.go
+++ cmd/auth-server/main.go
@@ -9,31 +9,15 @@ import (
 )
 
 const (
-	jwksPath  = "/app/config/jwks.json"
 	rulesPath = "/app/config/rules.json"
 )
 
 func main() {
 	app := gofr.New()
 
-	app.GET("/jwks.json", JWKSHandler)
 	app.GET("/rules.json", RulesHandler)
 
 	app.Run()
-}
-
-func JWKSHandler(c *gofr.Context) (interface{}, error) {
-	data, err := os.ReadFile(jwksPath)
-	if err != nil {
-		return nil, err
-	}
-
-	var jsonObj interface{}
-	if err := json.Unmarshal(data, &jsonObj); err != nil {
-		return nil, err
-	}
-
-	return response.Raw{Data: jsonObj}, nil
 }
 
 func RulesHandler(c *gofr.Context) (interface{}, error) {
