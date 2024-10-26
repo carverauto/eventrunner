c8c809432a6b246e3cb64cb470ba45a15412d8f6
mfreeman451 <mfreeman451@gmail.com>
2024-10-25 20:01:55 -0500

    🔧 jwks server for rules and jwks.json working

--- cmd/jwks-server/main.go
+++ cmd/jwks-server/main.go
@@ -1,46 +1,39 @@
-// File: main.go
-
 package main
 
 import (
+	"os"
+
 	"gofr.dev/pkg/gofr"
 )
 
+const (
+	jwksPath  = "/app/config/jwks.json"
+	rulesPath = "/app/config/rules.json"
+)
+
 func main() {
-	// Initialize gofr application
 	app := gofr.New()
 
-	// Register the JWKS route
 	app.GET("/jwks.json", JWKSHandler)
+	app.GET("/rules.json", RulesHandler)
 
-	// Run the application
 	app.Run()
 }
 
-type JWK struct {
-	E   string `json:"e"`
-	Kid string `json:"kid"`
-	Kty string `json:"kty"`
-	N   string `json:"n"`
-}
-
-type JWKS struct {
-	Keys []JWK `json:"keys"`
-}
-
 func JWKSHandler(c *gofr.Context) (interface{}, error) {
-	// Create the JWKS data
-	jwks := JWKS{
-		Keys: []JWK{
-			{
-				E:   "AQAB",
-				Kid: "eventrunner-jwt",
-				Kty: "RSA",
-				N:   "viVXLTzUz5zrrTRFe59lc5JfjonbmnBxgGVD2RHG-FQXdKp-5xnuH5C9ZLujcew8jYoeFw6o7ab7PMONzru5UcjxadKXaC1uTId_chCDVVVSD80IlYtzgchhMBTpqZJY5hd6GybODwJj0ulcfpXmw43dF5CRC9uLbLuSvkVsELgcioUJnaMTZjisY9R5ApeUOLSAZGOacdlVBBZfQb8pVjBqJQQmcyzooLZdXq-hNvutnI15sPQLcoBXXat_n8lfrI2Jr_mlG_rcvAdhZXUGeu1NeWdJuaHFoHcbV-PeSnr0mAGZxFEdM6nFywqmjtiU3EXhDmqfrB7hMiWdbAueRQ",
-			},
-		},
+	data, err := os.ReadFile(jwksPath)
+	if err != nil {
+		return nil, err
 	}
 
-	// Return the JWKS data; gofr will marshal it to JSON
-	return jwks, nil
+	return string(data), nil
+}
+
+func RulesHandler(c *gofr.Context) (interface{}, error) {
+	data, err := os.ReadFile(rulesPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return string(data), nil
 }
