04f4597bf9a5091d3e5b1d2beb48f1107a31378f
Michael Freeman <mfreeman451@gmail.com>
2024-11-22 11:58:56 -0600

    🔧 sync for 1file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,9 +4,12 @@ package main
 
 import (
 	"context"
+	"os"
 	"time"
 
+	"github.com/carverauto/eventrunner/pkg/api/handlers"
 	"github.com/carverauto/eventrunner/pkg/api/middleware"
+	ory "github.com/ory/client-go"
 	"gofr.dev/pkg/gofr"
 	"gofr.dev/pkg/gofr/datasource/mongo"
 )
@@ -34,27 +37,22 @@ func main() {
 	}
 
 	// Initialize Ory client
-	/*
-		oryClient := ory.NewConfiguration()
-		oryClient.Servers = ory.ServerConfigurations{{URL: os.Getenv("ORY_SDK_URL")}}
-		// oryClient.DefaultHeader["Authorization"] = "Bearer " + os.Getenv("ORY_PAT")
+	oryClient := ory.NewConfiguration()
+	oryClient.Servers = ory.ServerConfigurations{{URL: os.Getenv("ORY_SDK_URL")}}
+	// oryClient.DefaultHeader["Authorization"] = "Bearer " + os.Getenv("ORY_PAT")
 
-		apiClient := ory.NewAPIClient(oryClient)
+	apiClient := ory.NewAPIClient(oryClient)
 
-		// Initialize handlers
-		h := handlers.NewHandlers(apiClient)
-	*/
+	// Initialize handlers
+	h := handlers.NewHandlers(apiClient)
 
 	// Add debug logging middleware
 
 	// Set up routes
-	/*
-		app.POST("/api/users", middleware.Adapt(h.CreateUser, middleware.RequireRole("superuser", "tenant_admin")))
-		app.POST("/api/tenants/:tenant_id/users", middleware.Adapt(h.CreateUser, middleware.RequireRole("superuser", "tenant_admin")))
+	app.POST("/api/users", middleware.Adapt(h.CreateUser, middleware.RequireRole("superuser", "tenant_admin")))
+	app.POST("/api/tenants/{tenant_id}/users", middleware.Adapt(h.CreateUser, middleware.RequireRole("superuser", "tenant_admin")))
 
-		app.GET("/api/tenants/:tenant_id/users",
-			debugMiddleware(middleware.Adapt(h.GetAllUsers, middleware.RequireRole("superuser", "tenant_admin"))))
-	*/
+	app.GET("/api/tenants/{tenant_id}/users", middleware.Adapt(h.GetAllUsers, middleware.RequireRole("superuser", "tenant_admin")))
 
 	// Add the debug headers middleware
 	app.UseMiddleware(middleware.DebugHeadersMiddleware())
@@ -62,9 +60,7 @@ func main() {
 	// Add other middleware and routes
 	app.UseMiddleware(middleware.CustomHeadersMiddleware())
 
-	// Routes...
-	// app.POST("/api/v1/users", handlers.CreateUser)
-	// Add a callback handler to your routes
+	// this endpoint is used by the Ory Kratos login flow
 	app.GET("/callback", func(ctx *gofr.Context) (interface{}, error) {
 		code := ctx.Request.Param("code")
 		state := ctx.Request.Param("state")
